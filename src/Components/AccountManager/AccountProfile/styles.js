a1babd95f7666199262b5d99a285350179994000
Jose Luis Cullcay <jcullcay@tangocode.com>
2019-02-20 20:05:28 -0500

    Styles

--- src/Components/AccountManager/AccountProfile/styles.js
+++ src/Components/AccountManager/AccountProfile/styles.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const AccountProfile = styled.div`
     width: 100%;
-    height: 50vh;
+    height: 100vh;
     display: flex;
     background-color: #f8f8f8;
     flex-direction: column;
-    justify-content: space-around;
+    justify-content: start;
     align-items: center;
     padding-top: 20px;
 `;
@@ -25,4 +25,5 @@ export const AccountUpdate = styled.div`
     background-color: #d3d3d3;
     border: solid 1px black;
     padding: 20px;
+    margin-top: 20px;
 `;
